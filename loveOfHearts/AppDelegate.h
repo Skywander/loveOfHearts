82246e49dd628e4a7a9e9f0d3f15f0c0218ebc81
Skywander <c132316@163.com>
2016-03-10 09:03:58 +0800

    Initial Commit

---
+++ loveOfHearts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loveOfHearts
+//
+//  Created by 于恩聪 on 16/3/10.
+//  Copyright © 2016年 于恩聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ loveOfHearts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  loveOfHearts
+//
+//  Created by 于恩聪 on 16/3/10.
+//  Copyright © 2016年 于恩聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
